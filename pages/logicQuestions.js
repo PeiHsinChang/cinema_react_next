136d4cbdf1809e84a9b98efccf738709e24c9ace
peihsin <zottycute@gmail.com>
2022-07-17 17:34:09 +0800

    to center

--- pages/logicQuestions.js
+++ pages/logicQuestions.js
@@ -73,7 +73,7 @@ const LogicQuestions = () => {
 
     const modalAlert = document.getElementById("modalAlert");
     if (isOpenModal) {
-      modalAlert.style.display = "block";
+      modalAlert.style.display = "flex";
     } else {
       modalAlert.style.display = "none";
     }
