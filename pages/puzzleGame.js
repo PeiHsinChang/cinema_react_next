fb3bd5fa7a7f7390b259845fef7209c68fd525f8
peihsinchang <peihsinchang@eztravel.com.tw>
2022-07-21 16:56:53 +0800

    puzzleGame

---
+++ pages/puzzleGame.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import styles from "./PuzzleGame.module.scss";
+
+const Boxes = ({ boxCount, clickCount, onClick }) => {
+  let boxes = [];
+
+  const BoxComponent = ({ index }) => {
+    return (
+      <div
+        id={`box${index}`}
+        className={styles.box}
+        onClick={(e) => {
+          document.getElementById(e.target.id).innerHTML = clickCount;
+          onClick();
+
+          console.log(document.getElementById(e.target.id).innerHTML);
+        }}
+      >
+        {clickCount}
+      </div>
+    );
+  };
+  for (let i = 1; i <= boxCount; i++) {
+    boxes.push(<BoxComponent index={i} key={`box${i}`}></BoxComponent>);
+  }
+  return boxes;
+};
+
+const PuzzleGame = () => {
+  const [clickCount, setClickCount] = useState(0);
+  let boxCount = Math.pow(3, 2);
+
+  return (
+    <div>
+      map
+      <Boxes
+        boxCount={boxCount}
+        clickCount={clickCount}
+        onClick={() => setClickCount(clickCount + 1)}
+      />
+    </div>
+  );
+};
+
+export default PuzzleGame;
