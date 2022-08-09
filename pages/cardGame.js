c04d5e40bf36a23403dbed16550fa1a330bf8f9d
peihsinchang <peihsinchang@eztravel.com.tw>
2022-08-09 16:47:37 +0800

    logic match value

--- pages/cardGame.js
+++ pages/cardGame.js
@@ -17,31 +17,70 @@ function quickSort(arr) {
 }
 
 const test = quickSort([48, 44, 22, 99, 77, 32]);
-console.log("test", test);
 
 const Cards = ({ data }) => {
-  const [cardClassName, setCardClassName] = useState(`${styles.card}`);
-  const [cardsData, setCardsData] = useState([]);
+  const [cardsData, setCardsData] = useState(data);
+  const [openCardsCount, setOpenCardsCount] = useState(0);
+  const [matchValue, setMatchValue] = useState("");
+  const [selectData, setSelectData] = useState([]);
 
-  console.log("Cards", data);
-  const clickCard = (value) => {
-    console.log(value.target.dataset.value);
-    setCardClassName((cardClassName += ` ${styles.clickCard}`));
+  const cardClassName = styles.card;
+  const clickCardStyle = styles.clickCard;
+
+  useEffect(() => {
+    if (selectData.length === 2) {
+      // setOpenCardsCount(0);
+
+      /** 1. 先filter isOpen = true
+       *  2. 是否match? isMatch的style: isOpen的style */
+      const isMatchCards = selectData.every((item) => item === matchValue);
+      console.log("isMatchCards", isMatchCards);
+      setMatchValue("");
+
+      const newCardsData = cardsData
+        .filter((item) => item.value == matchValue)
+        .map((item) => {
+          isMatchCards ? (item.isMatch = true) : (item.isOpen = false);
+          // matchValue ? "" : (item.isOpen = false);
+          return {
+            ...item,
+          };
+        });
+      setSelectData([]);
+    }
+  }, [cardsData, selectData]);
+
+  const clickCard = (data) => {
+    const { cardIndex, value } = data.target.dataset;
+    const cardData = cardsData[parseInt(cardIndex)];
+    cardData.isOpen = true;
+    /** setCardsData不可以使用直接放cardsData，要[...cardsData] */
+    setCardsData([...cardsData]);
+
+    setOpenCardsCount((openCardsCount += 1));
+    selectData.push(value);
+    setSelectData(selectData);
+    if (selectData.length == 1) setMatchValue(value);
   };
-  const card = (data, index) => (
-    <div
-      key={index}
-      className={cardClassName}
-      onClick={(e) => clickCard(e)}
-      data-value={data.value}
-    >
-      {data.value}
-    </div>
-  );
-  return data.map((item, index) => card(item, index));
+
+  const card = (data, index) => {
+    return (
+      <div
+        key={index}
+        className={`${cardClassName} ${data.isOpen ? clickCardStyle : ""}`}
+        onClick={(e) => (data.isOpen ? () => {} : clickCard(e))}
+        data-value={data.value}
+        data-card-index={index}
+      >
+        {data.value}
+      </div>
+    );
+  };
+
+  return cardsData.map((item, index) => card(item, index));
 };
+
 function cardGame({ data }) {
-  console.log("cardGame", data);
   return (
     <div className={styles.container}>
       <Cards data={data} />
@@ -53,13 +92,16 @@ export async function getServerSideProps() {
   // Fetch data from external API
   // const res = await fetch(`https://.../data`);
   // const data = await res.json();
-  let data = [{ value: 1 }, { value: 2 }, { value: 3 }];
+  let data = [
+    { value: 1, isOpen: false, isMatch: false },
+    { value: 2, isOpen: false, isMatch: false },
+    { value: 3, isOpen: false, isMatch: false },
+  ];
   let data2 = [...data, ...data];
 
   data2.sort(() => {
     return 0.5 - Math.random();
   });
-  console.log(data2);
 
   // Pass data to the page via props
   return { props: { data: data2 } };
