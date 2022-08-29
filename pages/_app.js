9aefbfefbf8181deeb450ae02066b2395fb8a6cd
peihsinchang <peihsinchang@eztravel.com.tw>
2022-08-29 21:03:10 +0800

    build fail

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,11 @@
  * */
 import "../styles/global.scss";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />;
