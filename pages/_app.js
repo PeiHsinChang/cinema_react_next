8c732fc5698858166ddb8d0d993fbb289a4ddbd0
peihsinchang <peihsinchang@eztravel.com.tw>
2023-03-10 15:00:17 +0800

    test no script

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,14 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script language="javascript">
+          var noPrint = true; var noCopy = true; var noScreenshot = true; var
+          autoBlur = true;
+        </script>
+        <script
+          type="text/javascript"
+          src="https://pdfanticopy.com/noprint.js"
+        ></script>
       </Head>
       <Component {...pageProps} />;
     </>
