9be89da25bf5e87fb21651616c65a99ce33345e2
peihsin <zottycute@gmail.com>
2024-10-23 14:20:15 +0800

    feat: TEST RECAPTCHA_KEY

--- pages/reCAPTCHA.js
+++ pages/reCAPTCHA.js
@@ -1,64 +1,22 @@
-// import Head from "next/head";
-// import Script from "next/script";
-// import react from "react";
-import dynamic from "next/dynamic";
 import ReCAPTCHA from "react-google-recaptcha";
-
-// const NoSSR = dynamic(() => Captcha, { ssr: false });
 
 function onChange(value) {
   console.log("Captcha value:", value);
 }
 
-const Captcha = () => {
+const Page = ({ RECAPTCHA_KEY }) => {
   return (
     <>
-      {/* <Head>
-        <title>reCAPTCHA demo: Simple page</title>
-        <Script
-          src="https://www.google.com/recaptcha/api.js"
-          async
-          defer
-        ></Script>
-        <Script>
-          {function onSubmit(token) {
-            document.getElementById("demo-form").submit();
-          }}
-        </Script>
-      </Head>
-      <body>
-        <form id="demo-form" action="?" method="POST">
-          <button
-            className="g-recaptcha"
-            data-sitekey="6LeFZlQqAAAAAMjKM35xkFCr3yqtYaPeaPDCfQrd"
-            data-callback="onSubmit"
-          >
-            Submit
-          </button>
-          <br />
-        </form>
-      </body> */}
-
-      <ReCAPTCHA
-        sitekey="6LeFZlQqAAAAAMjKM35xkFCr3yqtYaPeaPDCfQrd"
-        onChange={onChange}
-      />
+      TEST RECAPTCHA_KEY
+      <ReCAPTCHA sitekey={RECAPTCHA_KEY} onChange={onChange} />
     </>
   );
 };
-
-const Page = () => {
-  return (
-    <>
-      {/* <NoSSR /> */}
-      <Captcha />
-    </>
-  );
-};
-
-// ReactDOM.render(
-//   ,
-//   document.body
-// );
 
 export default Page;
+
+export async function getStaticProps() {
+  const RECAPTCHA_KEY = process.env.RECAPTCHA_KEY;
+
+  return { props: { RECAPTCHA_KEY } };
+}
